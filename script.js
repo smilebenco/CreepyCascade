382be99ac264d99040a5579d6a883993cafd6c20
smilebenco <136312651+smilebenco@users.noreply.github.com>
2024-10-22 11:20:01 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,42 @@
+  const images = [
+    "image/png/2024-10/frankenstein-icon.png",
+    "image/png/2024-10/scream-icon.png",
+    "image/png/2024-10/grimacing-pumpkin-icon.png"
+];
+
+function createFallingImage() {
+    const img = document.createElement("img");
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    img.src = randomImage;
+
+    const size = Math.random() * (150 - 50) + 50;
+    img.style.width = size + "px";
+    img.style.height = size + "px";
+
+    const left = Math.random() * (window.innerWidth - size);
+    img.style.left = left + "px";
+
+    img.className = "falling-image";
+    document.body.appendChild(img);
+
+    setTimeout(() => {
+        img.style.top = window.innerHeight + "px";
+    }, 10);
+
+    img.addEventListener("transitionend", () => {
+        img.remove();
+    });
+}
+
+function removeOffscreenImages() {
+    const images = document.querySelectorAll('.falling-image');
+    images.forEach(img => {
+        const rect = img.getBoundingClientRect();
+        if (rect.top > window.innerHeight) {
+            img.remove();
+        }
+    });
+}
+
+setInterval(createFallingImage, 500);
+setInterval(removeOffscreenImages, 500);
